360d054853830460f49267471f0da4294e78a1b1
seperoph99 <lastgenmedia@gmail.com>
2021-09-27 17:23:07 -0400

    test1

--- BabyBear/bot.py
+++ BabyBear/bot.py
@@ -25,7 +25,7 @@ import json
 from datetime import datetime
 from dislash import InteractionClient, ActionRow, Button, ButtonStyle,SelectMenu, SelectOption
 
-#test Comment for gits
+#test Comment for gitsdsdfgsdfgsdfg
 @commands.command()
 async def on_ready():
     print("Everything's all ready to go~")
